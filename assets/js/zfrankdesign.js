21f4e0ede75daeb4d5e0ca5194f3fd87bb081338
Zach Frank <zfrankdesign@gmail.com>
2016-05-12 17:50:29 -0500

    First build.

--- assets/js/zfrankdesign.js
+++ assets/js/zfrankdesign.js
@@ -1,7 +1,7 @@
-(function($) {
+// (function($) {
+//
+//   // $(document).foundation();
+//
+// })( jQuery );
 
-  // $(document).foundation();
-
-})( jQuery );
-
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInpmcmFua2Rlc2lnbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiemZyYW5rZGVzaWduLmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uKCQpIHtcblxuICAvLyAkKGRvY3VtZW50KS5mb3VuZGF0aW9uKCk7XG5cbn0pKCBqUXVlcnkgKTtcbiJdLCJzb3VyY2VSb290IjoiL3NvdXJjZS8ifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInpmcmFua2Rlc2lnbi5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiemZyYW5rZGVzaWduLmpzIiwic291cmNlc0NvbnRlbnQiOlsiLy8gKGZ1bmN0aW9uKCQpIHtcbi8vXG4vLyAgIC8vICQoZG9jdW1lbnQpLmZvdW5kYXRpb24oKTtcbi8vXG4vLyB9KSggalF1ZXJ5ICk7XG4iXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
